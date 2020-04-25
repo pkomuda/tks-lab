e605ca7dba2175c65890249aecbdc619a672bf7b
216802 Komuda Przemysław <216802@edu.p.lodz.pl>
2020-04-25 15:53:59 +0200

    Add test dependencies and dockerfile

---
+++ rest/restservices/src/test/java/pl/lodz/p/it/tks/rest/tests/GetBooksTest.java
@@ -0,0 +1,50 @@
+package pl.lodz.p.it.tks.rest.tests;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.Test;
+import org.testcontainers.containers.GenericContainer;
+import org.testcontainers.containers.Network;
+import org.testcontainers.containers.output.Slf4jLogConsumer;
+import org.testcontainers.containers.wait.strategy.Wait;
+import org.testcontainers.images.builder.ImageFromDockerfile;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+import pl.lodz.p.it.model.catalogs.BookWeb;
+
+import javax.ws.rs.client.Client;
+import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.client.WebTarget;
+import javax.ws.rs.core.GenericType;
+import javax.ws.rs.core.MediaType;
+import java.nio.file.Path;
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+
+@Slf4j
+@Testcontainers
+public class GetBooksTest {
+
+    @Container
+    private static GenericContainer serviceOne = new GenericContainer(
+            new ImageFromDockerfile()
+                    .withDockerfileFromBuilder(builder
+                            -> builder
+                            .from("payaramicro11")
+                            .copy("restservices.war", "/opt/payara/deployments")
+                            .build())
+                    .withFileFromPath("restservices.war", Path.of("target", "restservices-1.0-SNAPSHOT.war"))
+    )
+            .withNetwork(Network.SHARED)
+            .withExposedPorts(8080, 4848, 6900)
+            .waitingFor(Wait.forHttp("/restservices-1.0-SNAPSHOT").forPort(8080).forStatusCode(200))
+            .withLogConsumer(new Slf4jLogConsumer(log));
+
+    @Test
+    public void testServiceOne() {
+        Client client = ClientBuilder.newClient();
+        WebTarget base = client.target("http://localhost:8080/restservices-1.0-SNAPSHOT/resources/api");
+        List<BookWeb> books = base.path("books").request(MediaType.APPLICATION_JSON).get(new GenericType<>() {});
+        assertEquals(books.size(), 3);
+    }
+}
