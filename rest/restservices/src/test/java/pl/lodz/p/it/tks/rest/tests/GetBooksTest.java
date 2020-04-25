12d49dbd111403fefa9700a1bb9b61aea889b14d
216802 Komuda Przemysław <216802@edu.p.lodz.pl>
2020-04-25 20:34:33 +0200

    Set context root for restservices module

--- rest/restservices/src/test/java/pl/lodz/p/it/tks/rest/tests/GetBooksTest.java
+++ rest/restservices/src/test/java/pl/lodz/p/it/tks/rest/tests/GetBooksTest.java
@@ -1,10 +1,8 @@
 package pl.lodz.p.it.tks.rest.tests;
 
 import lombok.extern.slf4j.Slf4j;
-import org.jboss.resteasy.client.jaxrs.ResteasyClientBuilder;
 import org.junit.Test;
 import org.testcontainers.containers.GenericContainer;
-import org.testcontainers.containers.Network;
 import org.testcontainers.containers.output.Slf4jLogConsumer;
 import org.testcontainers.containers.wait.strategy.Wait;
 import org.testcontainers.images.builder.ImageFromDockerfile;
@@ -13,6 +11,7 @@ import org.testcontainers.junit.jupiter.Testcontainers;
 import pl.lodz.p.it.model.catalogs.BookWeb;
 
 import javax.ws.rs.client.Client;
+import javax.ws.rs.client.ClientBuilder;
 import javax.ws.rs.client.WebTarget;
 import javax.ws.rs.core.GenericType;
 import javax.ws.rs.core.MediaType;
@@ -25,26 +24,29 @@ import static org.junit.Assert.assertEquals;
 @Testcontainers
 public class GetBooksTest {
 
+    private static Client client = ClientBuilder.newClient();
+
     @Container
-    private static GenericContainer serviceOne = new GenericContainer(
+    public static GenericContainer serviceOne = new GenericContainer(
             new ImageFromDockerfile()
                     .withDockerfileFromBuilder(builder
                             -> builder
-                            .from("payaramicro11")
+                            .from("payara/micro:jdk11")
                             .copy("restservices.war", "/opt/payara/deployments")
                             .build())
                     .withFileFromPath("restservices.war", Path.of("target", "restservices-1.0-SNAPSHOT.war"))
     )
-            .withNetwork(Network.SHARED)
             .withExposedPorts(8080, 4848, 6900)
-            .waitingFor(Wait.forHttp("/restservices/resources/javaee8").forPort(8080).forStatusCode(200))
+            .waitingFor(Wait.forHttp("/payararest/resources/javaee8").forPort(8080).forStatusCode(200))
             .withLogConsumer(new Slf4jLogConsumer(log));
 
     @Test
     public void testServiceOne() {
-        Client client = ResteasyClientBuilder.newClient();
-        WebTarget base = client.target("http://localhost:8080/restservices/resources/api");
+        serviceOne.start();
+        Integer port = serviceOne.getMappedPort(8080);
+        WebTarget base = client.target("http://localhost:" + port + "/payararest/resources/api");
         List<BookWeb> books = base.path("books").request(MediaType.APPLICATION_JSON).get(new GenericType<>() {});
         assertEquals(3, books.size());
+        serviceOne.stop();
     }
 }
