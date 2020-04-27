224bdafa6b8ef1898fec927748a3e6f61a67630d
Szymon Rutkowski <s.rutkowski235@gmail.com>
2020-04-27 15:03:50 +0200

    Book Test Rest api working

--- rest/restservices/src/test/java/pl/lodz/p/it/tks/rest/tests/GetBooksTest.java
+++ rest/restservices/src/test/java/pl/lodz/p/it/tks/rest/tests/GetBooksTest.java
@@ -1,5 +1,8 @@
 package pl.lodz.p.it.tks.rest.tests;
 
+import io.restassured.RestAssured;
+import io.restassured.path.json.JsonPath;
+import io.restassured.specification.RequestSpecification;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.Assert;
 import org.junit.Before;
@@ -13,8 +16,10 @@ import org.testcontainers.images.builder.ImageFromDockerfile;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
 import pl.lodz.p.it.model.catalogs.BookWeb;
+import pl.lodz.p.it.tks.rest.domainmodel.catalogs.Book;
 import pl.lodz.p.it.tks.rest.model.BookRestModel;
 
+import javax.swing.text.html.parser.Parser;
 import javax.ws.rs.client.Client;
 import javax.ws.rs.client.ClientBuilder;
 import javax.ws.rs.client.WebTarget;
@@ -23,8 +28,12 @@ import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import java.nio.file.Path;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
+import static io.restassured.RestAssured.given;
+import static io.restassured.RestAssured.when;
+import static org.hamcrest.CoreMatchers.hasItem;
 import static org.junit.Assert.assertEquals;
 
 @Slf4j
@@ -63,6 +72,7 @@ public class GetBooksTest {
         Assert.assertEquals(200,r.getStatus());
     }
 
+
     @Test
     public void getMoviesTest(){
         Response r = target
@@ -72,16 +82,50 @@ public class GetBooksTest {
         Assert.assertEquals(r.getStatus(),200);
     }
 
-//    @Test
-//    public void checkBooksAmount(){
+    @Test
+    public void checkBooksAmount(){
+//        JsonPath jsonPath = given()
+//                .when()
+//                .get("http://localhost:" + port8080 +"/payararest/resources/api/books")
+//                .then()
+//                .assertThat()
+//                .statusCode(Response.Status.OK.getStatusCode())
+//                .assertThat()
+//                .extract().body().jsonPath();
+
+//        List<BookRestModel> returnedbooks = jsonPath.getList("",BookRestModel.class);
+        BookRestModel[] books = given()
+                .contentType(MediaType.APPLICATION_JSON)
+                .when().get("http://localhost:" + port8080 +"/payararest/resources/api/books").as(BookRestModel[].class);
+//        Response r = (Response) given()
+//                .contentType(MediaType.APPLICATION_JSON)
+//                .when()
+//                .get("http://localhost:" + port8080 +"/payararest/resources/api/books")
+//                .then()
+//                .body("", hasItem("The Shining"));
+//        assertEquals(200,r.getStatus());
+//        for(int i=0; i<3; i++){
+//            System.out.println(returnedbooks.get(i).getAuthor());
+//        }
 //        ArrayList<BookRestModel> books = client
 //                .target("http://localhost:" + port8080 +"/payararest/resources/api")
 //                .path("books")
 //                .request(MediaType.APPLICATION_JSON)
 //                .get(new GenericType<>(){});
-//        Assert.assertEquals(books.size(),3);
-//    }
+        Assert.assertEquals(3,books.length);
+        Assert.assertEquals("The Shining",books[0].getTitle());
+    }
 
+    @Test
+    public void addBookRest(){
+        RestAssured.baseURI = "http://localhost:" + port8080 +"/payararest/resources/api/book";
+        RequestSpecification requestSpecification =  given();
+//                .body("book.json")
+//                .contentType(MediaType.APPLICATION_JSON)
+//                .post("http://localhost:" + port8080 +"/payararest/resources/api/book");
+//        assertEquals(200,.getStatus());
+
+    }
 
     @AfterAll
     public static void end(){
