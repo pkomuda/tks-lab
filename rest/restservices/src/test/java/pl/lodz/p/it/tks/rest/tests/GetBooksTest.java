2ce7612ee7697233b60e003261c856f00256903f
Szymon Rutkowski <s.rutkowski235@gmail.com>
2020-04-27 20:55:55 +0200

    I guess it works

--- rest/restservices/src/test/java/pl/lodz/p/it/tks/rest/tests/GetBooksTest.java
+++ rest/restservices/src/test/java/pl/lodz/p/it/tks/rest/tests/GetBooksTest.java
@@ -1,9 +1,12 @@
 package pl.lodz.p.it.tks.rest.tests;
 
 import io.restassured.RestAssured;
+import io.restassured.config.HttpClientConfig;
+import io.restassured.config.RestAssuredConfig;
 import io.restassured.http.ContentType;
 import io.restassured.response.Response;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.http.params.CoreConnectionPNames;
 import org.junit.Assert;
 import org.junit.jupiter.api.*;
 import org.testcontainers.containers.GenericContainer;
@@ -33,6 +36,10 @@ public class GetBooksTest {
     private static Integer port8080;
     private static WebTarget target;
     private static String path;
+    RestAssuredConfig newConfig = RestAssured.config()
+            .httpClient(HttpClientConfig.httpClientConfig()
+                    .setParam(CoreConnectionPNames.CONNECTION_TIMEOUT, 2000)
+                    .setParam(CoreConnectionPNames.SO_TIMEOUT, 2000));
     @Container
     public static GenericContainer serviceOne = new GenericContainer(
             new ImageFromDockerfile()
@@ -54,100 +61,97 @@ public class GetBooksTest {
         target = client.target("http://localhost:" + port8080 +"/payararest/resources/api");
         path = "http://localhost:" + port8080 +"/payararest/resources/api";
     }
-//    @Order(1)
-//    @Test
-//    public void getMovieTest() {
-//        MovieRestModel[] movies = RestAssured.given()
-//                .contentType(ContentType.JSON)
-//                .when()
-//                .get(path +"/movies")
-//                .as(MovieRestModel[].class);
-//        Assert.assertEquals(4, movies[1].getId());
-//        Assert.assertEquals("Trainspotting", movies[1].getTitle());
-//        Assert.assertEquals("Danny Boyle", movies[1].getDirector());
-//        Assert.assertEquals(1996, movies[1].getReleaseYear());
-//        Assert.assertEquals("DVD", movies[1].getFormat());
-//    }
-//
-//    @Order(2)
-//    @Test
-//    public void checkMoviesAmount() {
-//        MovieRestModel[] movies = RestAssured.given()
-//                .contentType(ContentType.JSON)
-//                .when()
-//                .get("http://localhost:" + port8080 +"/payararest/resources/api/movies")
-//                .as(MovieRestModel[].class);
-//        Assert.assertEquals(3, movies.length);
-//    }
-//
-//
-//
-//    @Order(3)
-//    @Test
-//    public void checkBooksAmount(){
-//        BookRestModel[] books = given()
-//                .contentType(MediaType.APPLICATION_JSON)
-//                .when().get(path+ "/books").as(BookRestModel[].class);
-//
-//        Assert.assertEquals(3,books.length);
-//        Assert.assertEquals("The Shining",books[0].getTitle());
-//        Assert.assertEquals("The Lord of the Rings", books[1].getTitle());
-//        Assert.assertEquals("What Could Possibly Go Wrong", books[2].getTitle());
-//    }
-//
-//
-//    public static io.restassured.response.Response putJsonBook() {
-//        File file = new File("F:/SEMESTR_V/TKS/tks_mkwa_czw_15_07/rest/restservices/src/test/java/pl/lodz/p/it/tks/rest/tests/bookupdate.json");
-//        BookRestModel bookRestModel = new BookRestModel(10,"A man with his dog","Bill Gates",1998);
-//        return
-//                given()
-//                        .contentType(ContentType.JSON)
-//                        .body(file)
-//                        .put("http://localhost:" + port8080 +"/payararest/resources/api/book/11")
-//                        .then()
-//                        .statusCode(200)
-//                        .extract()
-//                        .response();
-//
-//    }
+
+    @Order(1)
+    @Test
+    public void getMovieTest() {
+        MovieRestModel[] movies = RestAssured.given()
+                .config(newConfig)
+                .contentType(ContentType.JSON)
+                .when()
+                .get(path +"/movies")
+                .as(MovieRestModel[].class);
+        Assert.assertEquals(4, movies[1].getId());
+        Assert.assertEquals("Trainspotting", movies[1].getTitle());
+        Assert.assertEquals("Danny Boyle", movies[1].getDirector());
+        Assert.assertEquals(1996, movies[1].getReleaseYear());
+        Assert.assertEquals("DVD", movies[1].getFormat());
+    }
+
+    @Order(2)
+    @Test
+    public void checkMoviesAmount() {
+        MovieRestModel[] movies = RestAssured.given()
+                .config(newConfig)
+                .contentType(ContentType.JSON)
+                .when()
+                .get(path +"/movies")
+                .as(MovieRestModel[].class);
+        Assert.assertEquals(3, movies.length);
+    }
+
+    @Order(3)
+    @Test
+    public void checkBooksAmount(){
+        BookRestModel[] books = given()
+                .config(newConfig)
+                .contentType(ContentType.JSON)
+                .when()
+                .get(path+ "/books")
+                .as(BookRestModel[].class);
+
+        Assert.assertEquals(3,books.length);
+        Assert.assertEquals("The Shining",books[0].getTitle());
+        Assert.assertEquals("The Lord of the Rings", books[1].getTitle());
+        Assert.assertEquals("What Could Possibly Go Wrong", books[2].getTitle());
+    }
 
     @Order(4)
     @Test
-    public void addBookRest(){
+    public void addBookRest() throws InterruptedException {
         File file = new File("F:/SEMESTR_V/TKS/tks_mkwa_czw_15_07/rest/restservices/src/test/java/pl/lodz/p/it/tks/rest/tests/book.json");
         BookRestModel bookRestModel = new BookRestModel(10,"A man with his dog","Bill Gates",1998);
-              Response r = RestAssured.given()
+        Response r = RestAssured.given()
+                .config(newConfig)
+                .contentType(ContentType.JSON)
+                .body(file)
+                .post(path +"/book")
+                .then()
+                .statusCode(200)
+                .extract()
+                .response();
+        Assert.assertEquals(200,r.getStatusCode());
+        Thread.sleep(3000);
+    }
+
+    @Order(5)
+    @Test
+    public void putBookRest() throws InterruptedException {
+        File file = new File("F:/SEMESTR_V/TKS/tks_mkwa_czw_15_07/rest/restservices/src/test/java/pl/lodz/p/it/tks/rest/tests/book.json");
+        BookRestModel bookRestModel = new BookRestModel(10,"A man with his dog","Bill Gates",1998);
+        Response r = RestAssured.given()
+                .config(newConfig)
+                .contentType(ContentType.JSON)
+                .body(file)
+                .put(path +"/book/10")
+                .then()
+                .statusCode(200)
+                .extract()
+                .response();
+        Assert.assertEquals(200,r.getStatusCode());
+        Thread.sleep(3000);
+        io.restassured.response.Response response =
+                RestAssured.given()
+                        .config(newConfig)
                         .contentType(ContentType.JSON)
-                        .body(file)
-                        .post(path +"/book")
+                        .delete("http://localhost:" + port8080 +"/payararest/resources/api/catalog/10")
                         .then()
                         .statusCode(200)
                         .extract()
                         .response();
-        Assert.assertEquals(200,r.getStatusCode());
-
+        Assert.assertEquals(200,response.getStatusCode());
+        Thread.sleep(3000);
     }
-
-//    @Order(6)
-//    @Test
-//    public void putBookRest(){
-//        putJsonBook();
-//        Assert.assertEquals(200,putJsonBook().getStatusCode());
-//
-//    }
-//
-//    @Order(7)
-//    @Test
-//    public void deleteBook(){
-//        io.restassured.response.Response r =
-//                given()
-//                        .delete("http://localhost:" + port8080 +"/payararest/resources/api/catalog/11")
-//                        .then()
-//                        .statusCode(200)
-//                        .extract()
-//                        .response();
-//                Assert.assertEquals(200,r.getStatusCode());
-//    }
 
 
     @AfterAll
