f202da01287db5a5e19becb9693b747a8e0529e3
Szymon Rutkowski <s.rutkowski235@gmail.com>
2020-04-29 14:26:00 +0200

    FilterUserSoapService Tests

--- soap/soapservices/src/test/java/pl/lodz/p/it/tks/soap/client/tests/UserFilterSoapServiceTest.java
+++ soap/soapservices/src/test/java/pl/lodz/p/it/tks/soap/client/tests/UserFilterSoapServiceTest.java
@@ -1,4 +1,22 @@
 package pl.lodz.p.it.tks.soap.client.tests;
 
+import org.junit.jupiter.api.Test;
+import pl.lodz.p.it.tks.soap.client.ManagerSoap;
+import pl.lodz.p.it.tks.soap.client.UserFilterSoapServiceImpl;
+import pl.lodz.p.it.tks.soap.client.UserFilterSoapServiceImplService;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class UserFilterSoapServiceTest {
+
+    @Test
+    public void filterUsers(){
+        UserFilterSoapServiceImpl userFilterSoapService = new UserFilterSoapServiceImplService().getUserFilterSoapServiceImplPort();
+        List<ManagerSoap> managers = userFilterSoapService.filterManagers("Jimmy");
+        assertEquals("Jimmy",managers.get(0).getFirstName());
+    }
+
 }
+
