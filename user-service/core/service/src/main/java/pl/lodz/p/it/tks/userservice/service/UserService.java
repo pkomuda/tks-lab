fa60bdfe756a3700e209b98605dcf251c7b6d98c
216802 Komuda Przemysław <216802@edu.p.lodz.pl>
2020-05-12 22:00:56 +0200

    Add modules for user service

---
+++ user-service/core/service/src/main/java/pl/lodz/p/it/tks/userservice/service/UserService.java
@@ -0,0 +1,4 @@
+package pl.lodz.p.it.tks.userservice.service;
+
+public class UserService {
+}
---
+++ user-service/repo/src/main/java/pl/lodz/p/it/tks/userservice/repo/repository/UserRepository.java
@@ -0,0 +1,4 @@
+package pl.lodz.p.it.tks.userservice.repo.repository;
+
+public class UserRepository {
+}
