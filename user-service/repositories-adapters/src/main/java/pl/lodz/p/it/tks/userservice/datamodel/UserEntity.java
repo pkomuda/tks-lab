e34a9e6ff9cbd441b88a0ff1964a1eab8852acc1
216802 Komuda Przemysław <216802@edu.p.lodz.pl>
2020-05-14 01:10:22 +0200

    Add UserType enum

--- user-service/repositories-adapters/src/main/java/pl/lodz/p/it/tks/userservice/datamodel/UserEntity.java
+++ user-service/repositories-adapters/src/main/java/pl/lodz/p/it/tks/userservice/datamodel/UserEntity.java
@@ -1,4 +1,21 @@
 package pl.lodz.p.it.tks.userservice.datamodel;
 
-public class UserEntity {
+import lombok.Data;
+
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.Id;
+
+@Entity
+public @Data class UserEntity {
+
+    @Id
+    private String username;
+    private String password;
+    private String firstName;
+    private String lastName;
+    private boolean active;
+    @Enumerated(EnumType.STRING)
+    private UserType type;
 }
---
+++ user-service/repositories-adapters/src/main/java/pl/lodz/p/it/tks/userservice/datamodel/UserType.java
@@ -0,0 +1,5 @@
+package pl.lodz.p.it.tks.userservice.datamodel;
+
+public enum UserType {
+    ADMIN, MANAGER, CLIENT
+}
